79a1556606ae10b81dd94c6d48e10919d62a3305
Jon Lemmon <jon@loomio.org>
2013-09-19 15:09:43 +1200

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cucumbersauce::Application.config.secret_token = '6143fc872320d1993bfa949ed70eccef3cada6bf6a3ba1116de88248fe0a0c6d22d1a7645bb4316efb05254e61b740ed71d6e31731be64d9598c45bf088a8987'
